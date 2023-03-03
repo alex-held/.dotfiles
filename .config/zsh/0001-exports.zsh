0a919ce165fc0b6cfab843e82658f082e575582b
Alexander Held <git@alexheld.io>
2023-03-03 18:04:51 +0100

    refactor(zsh): move p10k to init section

--- .config/zsh/0001-exports.zsh
+++ .config/zsh/0001-exports.zsh
@@ -2,7 +2,12 @@
 
 export FPATH="$(brew --prefix)/share/zsh-completions:$FPATH"
 
-
+fpath=(
+    /opt/homebrew/share/zsh-completions
+    /opt/homebrew/share/zsh/site-functions
+    /opt/homebrew/Cellar/zsh/5.9/share/zsh/functions
+    /usr/local/share/zsh/site-functions
+)
 export KUBECONFIG=$(test -e $HOME/.kube && find $HOME/.kube -name "*.kubecfg" | tr '\n' ':' | rev | sed 's/^://' | rev)
 export GOPATH="$HOME/go"
 export FZF_BASE="$(brew --prefix)/opt/fzf"
