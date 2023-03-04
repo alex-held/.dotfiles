51b95e260a34bf1cd5bb0d30c8d9b33ba0a29792
Alexander Held <git@alexheld.io>
2023-03-04 17:33:01 +0100

    feat(zsh): add cd aliases

--- .config/zsh/0005-completions.zsh
+++ .config/zsh/0005-completions.zsh
@@ -1,8 +1,11 @@
 source ~/.config/shellutils.sh
 
 if command::exists "brew"; then
+
+
     # https://stackoverflow.com/a/22753363
-    compaudit | xargs chmod g-w
+    #compaudit | xargs chmod g-w
+    chmod -R go-w '/opt/homebrew/share/zsh'
 
     autoload -Uz compinit
     compinit
--- .config/zsh/0006-aliases.zsh
+++ .config/zsh/0006-aliases.zsh
@@ -4,3 +4,7 @@ alias vim="nvim"
 alias reload="source ~/.zshrc"
 
 alias l="exa --long --git -H -g -a --classify --group-directories-first --modified --octal-permissions"
+
+
+alias cdah="cd ~/source/repos/alex-held"
+alias cdr="cd ~/source/repos"
